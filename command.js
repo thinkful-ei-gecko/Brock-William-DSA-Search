251ef7363874553410f4be3d5d04a4406f5ca3b6
William Bae <wbae912@gmail.com>
2019-11-13 13:21:06 -0800

    Exercise 6 complete

--- command.js
+++ command.js
@@ -5,21 +5,32 @@ function nextCommander(bst, result = []){
   const queue = new Queue();
   queue.enqueue(bst);
 
-  while(queue.first !== null)
+  while(queue.first !== null) {
+    const person = queue.dequeue();
+    result.push(person.value);
+    if(person.left) {
+      queue.enqueue(person.left);
+    }
+    if(person.right) {
+      queue.enqueue(person.right);
+    }
+  }
+  return result;
 }
 
 function main() {
-  let BST = new BST();
+  let bst = new BST();
 
-  BST.insert(10, 'Picard'),
-  BST.insert(8, 'Riker'),
-  BST.insert(12, 'Data'),
-  BST.insert(9, 'Worf'),
-  BST.insert(14, 'Crusher'),
-  BST.insert(1, 'Security'),
-  BST.insert(16, 'Selar')
+  bst.insert(10, 'Picard');
+  bst.insert(9, 'Riker');
+  bst.insert(8, 'Data');
+  bst.insert(7, 'Worf');
+  bst.insert(6 ,'Laforge');
+  bst.insert(5, 'Crusher');
+  bst.insert(4, 'Security');
+  bst.insert(3, 'Selar');
 
-  console.log(nextCommander(BST));
+  console.log(JSON.stringify(nextCommander(bst)));
 
   // Picard, Riker, Data, Worf, Laforge, Crusher, Security, Selar
 
